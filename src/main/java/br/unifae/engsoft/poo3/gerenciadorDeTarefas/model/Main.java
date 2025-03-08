457c185722f523b90e9ace2543853e95aa4e8bec
JFNetto <jfnetto10@gmail.com>
2025-03-08 16:32:57 -0300

    Mudando algumas coisas

--- src/main/java/br/unifae/engsoft/poo3/gerenciadorDeTarefas/model/Main.java
+++ src/main/java/br/unifae/engsoft/poo3/gerenciadorDeTarefas/model/Main.java
@@ -5,7 +5,7 @@ package br.unifae.engsoft.poo3.gerenciadorDeTarefas.model;
 import br.unifae.engsoft.poo3.gerenciadorDeTarefas.view.TelaInicialView;
 
 
-public class GerenciadorDeTarefas {
+public class Main {
 
     public static void main(String[] args) {
         TelaInicialView ti = new TelaInicialView();
